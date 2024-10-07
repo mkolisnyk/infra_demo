c54af6f4a1d3987870908b733599be3df5c38ebb
mkolisnyk <kolesnik.nickolay@gmail.com>
2024-10-07 21:10:02 +0100

    Service app classes were added

---
+++ src/main/java/com/sample/app/model/TaxRequest.java
@@ -0,0 +1,13 @@
+package com.sample.app.model;
+
+public class TaxRequest {
+	private float income;
+
+	public float getIncome() {
+		return income;
+	}
+
+	public void setIncome(float income) {
+		this.income = income;
+	}
+}
--- src/test/java/com/sample/core/EngineTest.java
+++ src/test/java/com/sample/core/EngineTest.java
@@ -1,11 +1,9 @@
-package com.sample;
+package com.sample.core;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Arrays;
 import java.util.List;
@@ -16,12 +14,13 @@ public class EngineTest {
     private static List<Arguments> argumentSets() {
         return Arrays.asList(
                 arguments(0.f, 0.f),
-                arguments(25000.f, 1.f),
-                arguments(50000.f, 1.f),
-                arguments(51000.f, 1.f),
-                arguments(123000.f, 1.f),
-                arguments(128000.f, 1.f),
-                arguments(145000.f, 1.f)
+                arguments(25000.f, 2500.f),
+                arguments(50000.f, 7500.f),
+                arguments(51000.f, 7900.f),
+                arguments(123000.f, 36700.f),
+                arguments(124000.f, 39600.f),
+                arguments(128000.f, 41200.f),
+                arguments(150000.f, 50250.f)
         );
     }
 
@@ -30,6 +29,6 @@ public class EngineTest {
     @MethodSource("argumentSets")
     public void testEngineCalculate1250L(float income, float expectedTax) {
         Engine engine = new Engine();
-        Assertions.assertEquals(expectedTax, engine.calculate1250L(income));
+        Assertions.assertEquals(expectedTax, engine.calculateTax1250L(income));
     }
 }
