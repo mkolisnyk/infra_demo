1cd48b237ad59bec26d226bc52e048983f1a10be
mykola.kolisnyk <kolesnik.nickolay@gmail.com>
2024-10-10 18:18:48 +0100

    Calculation optimisations and new tax codes

--- src/main/java/com/sample/core/Engine.java
+++ src/main/java/com/sample/core/Engine.java
@@ -15,31 +15,25 @@ public class Engine {
         private static final long serialVersionUID = 1L;
 
         {
+            put(taxAllowance, 0.f);
             put(50000.f, 0.2f);
             put(153000.f, 0.4f);
+            put(Float.MAX_VALUE, 0.45f);
         }
     };
     public float calculateTax1250L(float income) {
         float tax = 0.f;
-        float taxedAmount = taxAllowance;
-        float lastBorder = 0;
-        if (income <= taxAllowance) {
-            return tax;
-        }
+        float taxedAmount = 0;
         if (income > taxAllowanceCap) {
-            taxedAmount = 0;
+            rates.remove(taxAllowance);
         }
         for (Entry<Float, Float> entry : rates.entrySet()) {
-            lastBorder = entry.getKey();
             float charge = (Math.min(entry.getKey(), income) - taxedAmount);
             tax += charge * entry.getValue();
             taxedAmount += charge;
             if (income <= entry.getKey()) {
                 break;
             }
-        }
-        if (income > lastBorder) {
-            tax += (income - lastBorder) * upperRate;
         }
         return tax;
     }
---
+++ src/test/java/com/sample/app/domain/TaxCodeTest.java
@@ -0,0 +1,29 @@
+package com.sample.app.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+public class TaxCodeTest {
+    private static List<Arguments> argumentSets() {
+        return Arrays.asList(
+                arguments("1257L", TaxCode.L, "1257"),
+                arguments("D0", TaxCode.D0, ""),
+                arguments("Something", TaxCode.UNKNOWN, "")
+        );
+    }
+
+    @ParameterizedTest(name = "Calculate tax for {0}")
+    @MethodSource("argumentSets")
+    public void testFromStringToTaxCode(String input, TaxCode expectedCode, String expectedValue) {
+        TaxCode actual = TaxCode.fromString(input);
+        Assertions.assertEquals(expectedCode, actual, "Unexpected code detected");
+        Assertions.assertEquals(expectedValue, actual.getValue(), "Unexpected value detected");
+    }
+}
